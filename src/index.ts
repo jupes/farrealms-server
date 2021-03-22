b7f83c61e2ccf8c9a366dce567905f26ddb6cf01
Erich Staehling <erich.staehling@gmail.com>
2021-03-22 14:52:06 -0400

    Successful server added that  is express, apollo is handling the middleware, graphQL is running on localhost and I can send queries and see a response, code in stable state

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,41 @@ import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import { Post } from './entities/Post';
 import mikroConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, { title: 'my first post' });
-  await orm.em.persistAndFlush(post);
-  //await orm.em.nativeInsert(Post, {'my first post 2'})
 
-    // this will let me see what posts are in the database later
-//   const posts = await orm.em.find(Post, {});
-//   console.log(posts);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log('server started on localhost:4000');
+  });
+
+  // underscore placed at the request param as best practice
+  //   app.get('/', (_, res) => {
+  //     res.send(posts);
+  //   });
+
+  //   const post = orm.em.create(Post, { title: 'my first post' });
+  //   await orm.em.persistAndFlush(post);
+
+  // this will let me see what posts are in the database later
+  //   const posts = await orm.em.find(Post, {});
+  //   console.log(posts);
 };
 
 main().catch((err) => {
