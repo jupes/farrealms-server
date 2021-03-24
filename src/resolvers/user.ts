479a9377177f64940b83440bc89142e810ec9e4d
Erich Staehling <erich.staehling@gmail.com>
2021-03-23 22:10:21 -0400

    added hasing with argon2 to user registered passwords, no plain txt for me, added a query to see the registered users

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,24 +1,36 @@
-import { User } from 'src/entities/User';
+import { User } from '../entities/User';
 import { MyContext } from 'src/types';
 import { Arg, Ctx, Field, InputType, Mutation, Query, Resolver } from 'type-graphql';
+import argon2 from 'argon2';
 
 @InputType()
 class UsernamePasswordInput {
-    @Field()
-    username: string
-    @Field()
-    password: string
+  @Field()
+  username: string;
+  @Field()
+  password: string;
 }
+
+
 
 @Resolver()
 export class UserResolver {
-  @Mutation(() => String)
+  @Mutation(() => User)
   async register(
-      @Arg('options', () => UsernamePasswordInput) options: UsernamePasswordInput,
-      @Ctx() { em }: MyContext
-  ) {
-      const user = em.create(User, {username: options.username})
-      await em.persistAndFlush(user)
-    return 'buttsw  ';
+    @Arg('options', () => UsernamePasswordInput) options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<User> {
+    const hashedPassword = await argon2.hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
+    await em.persistAndFlush(user);
+    return user;
+  }
+
+  @Query(() => [User])
+  users(@Ctx() { em }: MyContext): Promise<User[]> {
+    return em.find(User, {});
   }
 }
