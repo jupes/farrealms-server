447370666ab759bf0a7b0c7e68b3ed647f926beb
Erich Staehling <erich.staehling@gmail.com>
2021-03-25 21:56:49 -0400

    added login resolver with field errors, added a regex to ensure passwords meet standards

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,20 @@
 // the NODE_ENV is a special variable that is set to the string production in a production environment
 export const __prod__ = process.env.NODE_ENV === 'production';
+/* Regex for ensuring password meets the following requirements: 
+    8 to 30 characters length
+    2 letters in Upper Case
+    1 Special Character (!@#$&*)
+    2 numerals (0-9)
+    3 letters in Lower Case
+    --
+    ^                         Start anchor
+    (?=.*[A-Z].*[A-Z])        Ensure string has two uppercase letters.
+    (?=.*[!@#$&*])            Ensure string has one special case letter.
+    (?=.*[0-9].*[0-9])        Ensure string has two digits.
+    (?=.*[a-z].*[a-z].*[a-z]) Ensure string has three lowercase letters.
+    .{8, 30}                  Ensure string is of length 8 to 30.
+    $                         End anchor.
+*/
+export const __passwordRegex__ = new RegExp(
+  '^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8, 30}$'
+);
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,8 +1,18 @@
 import { User } from '../entities/User';
 import { MyContext } from 'src/types';
-import { Arg, Ctx, Field, InputType, Mutation, Query, Resolver } from 'type-graphql';
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+} from 'type-graphql';
 import argon2 from 'argon2';
 
+// Input types are used for arguments
 @InputType()
 class UsernamePasswordInput {
   @Field()
@@ -11,7 +21,22 @@ class UsernamePasswordInput {
   password: string;
 }
 
+// Object types are what we return
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
 
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
 
 @Resolver()
 export class UserResolver {
@@ -27,6 +52,38 @@ export class UserResolver {
     });
     await em.persistAndFlush(user);
     return user;
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg('options', () => UsernamePasswordInput) options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: 'The username entered does not exist',
+          },
+        ],
+      };
+    }
+
+    const validPassword = await argon2.verify(user.password, options.password);
+    if (!validPassword) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: 'The password entered does not match',
+          },
+        ],
+      };
+    }
+
+    return { user };
   }
 
   @Query(() => [User])
